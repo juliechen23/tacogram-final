6ec137cf7b65c146854306285e5912ff0a47f985
juliechen23 <julie.chen@kellogg.northwestern.edu>
2023-03-07 21:48:32 +0000

    ready to ship

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,10 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    respond_to do |format|
+      format.html
+      format.json {render :json =>@posts}
+    end
   end
 
   def new
